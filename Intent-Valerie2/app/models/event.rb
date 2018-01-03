f8d778477adcc8ef6b57422f9dfd9a36295b3d1d
vpkirby002 <vpkirby002@gmail.com>
2018-01-03 15:03:25 -0500

    Revert "calendar updates"
    This reverts commit 7d77d8fd3c3837b4bb6990bf2530e49a873734c3.

--- Intent-Valerie2/app/models/event.rb
+++ Intent-Valerie2/app/models/event.rb
@@ -1,11 +1,2 @@
 class Event < ApplicationRecord
-	  belongs_to :resource
-
-  validates :title, presence: true
-
-  attr_accessor :date_range
-
-  def all_day_event?
-    self.start == self.start.midnight && self.end == self.end.midnight ? true : false
-  end
 end
